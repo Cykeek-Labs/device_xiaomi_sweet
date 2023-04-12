abd4a2ecdcbafce4cf4e7de367f79a222d63ea24
Niranjan BR <niranjankannan2003@gmail.com>
2023-04-12 01:24:26 +0000

    sweet: Define board

--- device.mk
+++ device.mk
@@ -107,6 +107,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Define board
+PRODUCT_BOARD_PLATFORM := sm6150
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Device Settings
 PRODUCT_PACKAGES += \
     DeviceSettings
