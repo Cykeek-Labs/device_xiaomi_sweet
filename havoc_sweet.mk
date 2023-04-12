ecc5be15dc2df762bd85f67d8aef41e0cb197ecd
Cykeek <Cykeek@proton.me>
2023-04-12 16:43:09 +0530

    sweet: Track Gapps package if exist
    Signed-off-by: Cykeek <Cykeek@proton.me>

--- havoc_sweet.mk
+++ havoc_sweet.mk
@@ -25,3 +25,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+
+# Add Gapps package
+$(call inherit-product, vendor/gapps/config.mk)
