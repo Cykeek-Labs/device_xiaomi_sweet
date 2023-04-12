48bf77a7272974c54ba2c0c3e8d0383c1c4b97e7
Niranjan BR <niranjankannan2003@gmail.com>
2023-04-12 01:19:20 +0000

    sweet: Welcome Havoc OS

--- havoc_sweet.mk
+++ havoc_sweet.mk
@@ -6,20 +6,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit ANX camera
 $(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := havoc_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-DEVICE_MAINTAINER := daniml3
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
